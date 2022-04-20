dca2b69229076d06713912994486637cdff43f7f
Mukul Data <54991370+mukuldata@users.noreply.github.com>
2022-04-20 16:07:23 +0530

    Update Stacks.js

--- Stacks.js
+++ Stacks.js
@@ -1,1 +1,36 @@
 
+// 1. Duplicate Brackets: (EASY)
+
+class Main{
+   public static void main(String[] args){
+      Scanner scn=new Scanner(System.in);
+      String str= scn.nextLine();
+      Stack<Character> st=new Stack<>();
+      int ct=0;
+//       Checking if the characters are present inbetween brackets : If not present the duplicate brackets are there
+      for(int i=0;i<str.length();i++){
+         if(str.charAt(i)!=')'){
+            st.push(str.charAt(i));
+         }
+         else{
+            while(st.peek()!='('){
+               st.pop();
+               ct++;
+            }
+            if(ct>0){
+               st.pop();
+               ct=0;
+            }
+            else{
+               System.out.println("true");
+               return ;
+            }
+
+        }
+
+      }
+
+      System.out.println("false");
+
+   }
+}
